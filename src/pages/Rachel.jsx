885b1e2dc221cc5e0e87835c60e84217e8514592
sarahvar <sarahvarin95@gmail.com>
2024-10-21 14:27:35 +0200

    Rachel

--- src/pages/Rachel.jsx
+++ src/pages/Rachel.jsx
@@ -1,19 +1,26 @@
 import React from "react";
 import "./Rachel.css"; // Importez le fichier CSS
-
 const RachelPage = () => {
   return (
     <div>
       <main>
         {/* Exemple de h1 avec une classe spécifique */}
         <h1 className="rachel-heading">Rachel Dawn Amber</h1>
-
         {/* Conteneur d'images */}
         <div className="image-container">
-          <div className="rachelcenter"></div> {/* Image de Rachel */}
-          <div className="conteneur"></div> {/* Image de masque de théâtre */}
-        </div>
+          <div className="rachelcenter">
+            <img src="/dist/assets/Rachel-Amber-BTS.webp" alt="Rachel Amber" />
+          </div>
 
+          <div className="conteneur">
+            <img
+              className="alignRight"
+              src="/dist/assets/Masque-Théâtre.jpg"
+              height="200"
+              alt="Rachel Amber Art"
+            />
+          </div>
+        </div>
         {/* Biographie */}
         <section>
           <h2>Biographie de Rachel Amber</h2>
@@ -38,7 +45,11 @@ const RachelPage = () => {
             musicaux que par son style vestimentaire. Elle est intelligente et a
             un œil pour la photographie et l'art, tout comme Max. En considérant
             ses résultats scolaires et les efforts qu'elle a fait pour devenir
-            un modèle, cela fait d'elle une personne très ambitieuse.
+            un modèle, cela fait d'elle une personne très ambitieuse. Elle
+            semble avoir une personnalité bienveillante, puisqu'elle est
+            appréciée par ses proches. Rachel est plus girly et idéaliste que
+            Chloe, ceci se reflétant par son amour pour le maquillage et son
+            ambition de devenir mannequin.
           </p>
 
           <h3>Caractéristiques physiques :</h3>
@@ -54,14 +65,19 @@ const RachelPage = () => {
             par les nombreux avis de recherche déposés par Chloe.
           </p>
 
-          <div className="imgrachel"></div> {/* Image de l'avis de recherche */}
+          <div className="imgrachel">
+            <img
+              src="dist/assets/Recherche-Rachel-Amber.png"
+              height="400"
+              alt="Avis de recherche de Rachel Amber"
+            />
+          </div>
 
           <h3>Son rôle dans l'histoire:</h3>
           <ul>
             <li>Elle guide Max</li>
           </ul>
-
-          <div className="rachel-guide"></div> {/* Image de Rachel guidant Max */}
+          <img src="/dist/assets/Biche.jpg" alt="Rachel guidant Max" />
 
           <div className="centertext">
             <h2>Les personnages liés à Rachel:</h2>
@@ -79,7 +95,12 @@ const RachelPage = () => {
               <tr>
                 <td>
                   <b>Chloe, Elizabeth Price</b>
-                  <div className="chloe-image"></div> {/* Image de Chloe */}
+                  <br />
+                  <img
+                    src="/assets/Chloe-Price.jpg"
+                    height="100"
+                    alt="Chloe Price"
+                  />
                 </td>
                 <td>Elle est sa meilleure amie et amoureuse probablement</td>
                 <td>
@@ -89,7 +110,8 @@ const RachelPage = () => {
               <tr>
                 <td>
                   <b>Joyce Madsen</b>
-                  <div className="joyce-image"></div> {/* Image de Joyce */}
+                  <br />
+                  <img src="/assets/Joyce-Madsen.jpg" alt="Joyce Madsen" />
                 </td>
                 <td>Elle est la mère de Chloé, sa meilleure amie</td>
                 <td>
@@ -100,7 +122,12 @@ const RachelPage = () => {
               <tr>
                 <td>
                   <b>Victoria Chase</b>
-                  <div className="victoria-image"></div> {/* Image de Victoria */}
+                  <br />
+                  <img
+                    src="/assets/Victoria-Chase.webp"
+                    height="125"
+                    alt="Victoria Chase"
+                  />
                 </td>
                 <td>Elles sont "amies"</td>
                 <td>
@@ -111,7 +138,12 @@ const RachelPage = () => {
               <tr>
                 <td>
                   <b>Nathan, Joshua Prescott</b>
-                  <div className="nathan-image"></div> {/* Image de Nathan */}
+                  <br />
+                  <img
+                    src="/assets/Nathan-Prescott.png"
+                    height="125"
+                    alt="Nathan Prescott"
+                  />
                 </td>
                 <td>Rachel et lui traînent ensemble</td>
                 <td>Nathan est intéressé par Rachel</td>
@@ -119,7 +151,12 @@ const RachelPage = () => {
               <tr>
                 <td>
                   <b>Mr Jefferson</b>
-                  <div className="jefferson-image"></div> {/* Image de Mr Jefferson */}
+                  <br />
+                  <img
+                    src="/assets/Mark_jefferson.webp"
+                    height="125"
+                    alt="Mr Jefferson"
+                  />
                 </td>
                 <td>Il est professeur de photographie où Rachel étudie</td>
                 <td>Des rumeurs disent qu'ils ont eu une relation</td>
@@ -127,7 +164,12 @@ const RachelPage = () => {
               <tr>
                 <td>
                   <b>Frank Bowers</b>
-                  <div className="frank-image"></div> {/* Image de Frank */}
+                  <br />
+                  <img
+                    src="/assets/Frank-Bowers.webp"
+                    height="200"
+                    alt="Frank Bowers"
+                  />
                 </td>
                 <td>Le dealer</td>
                 <td>
@@ -152,10 +194,31 @@ const RachelPage = () => {
             Rachel a un tatouage de dragon sur son mollet droit. Le symbole du
             dragon est lié à la renommée, la réputation et la carrière ainsi que
             la force et la puissance, qui sont tous applicables à la
-            personnalité et à l'histoire de Rachel.
+            personnalité et à l'histoire de Rachel. Le symbole du dragon incarne
+            aussi le désir et la passion sexuelle. En outre, les dragons en
+            Orient sont associés à la sagesse, la longévité et la régénération,
+            ce qui se réfère à ses performances académiques et intellectuelles,
+            mais aussi à sa persistance dans les cœurs et les souvenirs de la
+            plupart des gens à Arcadia Bay et son omniprésence sur la ville. Le
+            dragon est également connu pour servir de puissant gardien et guide,
+            ce qui fait référence au rôle que Rachel a joué dans la vie de
+            Chloe, mais aussi aux conseils que Rachel fournit indirectement à
+            Max tout au long du jeu. Les dragons sont des messagers de
+            l'équilibre et de la magie, nous encourageant à voir le monde à
+            travers les yeux du mystère et de l'émerveillement, et ils sont
+            aussi l'incarnation du pouvoir primordial. En alchimie, le dragon
+            représente le chaos-monde qui contient un certain potentiel de
+            l'ordre; le dragon est alors symbole de l'énergie primordiale. Le
+            mot dragon veut dire "voir clair"; "regard perçant".
           </p>
 
-          <div className="tattoo-image"></div> {/* Image du tatouage de Rachel */}
+          <div className="center">
+            <img
+              src="/assets/Tatouage-de-Rachel.jpg"
+              height="400"
+              alt="Tatouage de Rachel"
+            />
+          </div>
 
           <h3>Mon opinion :</h3>
           <p>
