9804ab4cba6e7866ee8923ea16005e4a7dc8ed01
sarahvar <sarahvarin95@gmail.com>
2024-11-09 21:03:30 +0100

    QuizPage_cursor

--- src/pages/QuizPage.jsx
+++ src/pages/QuizPage.jsx
@@ -16,63 +16,22 @@ const shuffleArray = (array) => {
 
 // Liste des questions pour l'exemple
 const initialQuestions = [
-  {
-    question: 'Quel est le nom complet de Max ?',
-    answers: ['Maxine Caulfield', 'Max Caulfield', 'Maxine Carter', 'Max Carter'],
-    correct: 0
-  },
-  {
-    question: 'Quel est le super pouvoir de Max ?',
-    answers: ['Télékinésie', 'Voyage dans le temps', 'Télépathie', 'Invisibilité'],
-    correct: 1
-  },
-  {
-    question: 'Quel est le nom de l\'amie d\'enfance de Max ?',
-    answers: ['Rachel Amber', 'Victoria Chase', 'Kate Marsh', 'Chloé Price'],
-    correct: 3
-  },
-  {
-    question: 'Quel est le nom de l\'école que Max fréquente ?',
-    answers: ['Arcadia Bay School', 'Pacific Northwest School', 'Blackwell Academy', 'West Coast Academy'],
-    correct: 2
-  },
-  {
-    question: 'Comment s\'appelle la ville où se déroule l\'histoire ?',
-    answers: ['Arcadia Bay', 'Bright Falls', 'Twin Peaks', 'Silent Hill'],
-    correct: 0
-  },
-  {
-    question: 'Quelle est la passion artistique de Max ?',
-    answers: ['Peinture', 'Photographie', 'Écriture', 'Musique'],
-    correct: 1
-  },
-  {
-    question: 'Quelle est la principale cause des conflits entre Max et Chloé ?',
-    answers: ['Le déménagement de Max', 'Les secrets de Chloé', 'La disparition de Rachel', 'Les relations amoureuses'],
-    correct: 0
-  },
-  {
-    question: ' Pourquoi Max retourne-t-elle à Arcadia Bay après plusieurs années d\'absence ?',
-    answers: ['Pour renouer avec sa meilleure amie Chloé', 'Pour un projet de photographie', 'Pour suivre des cours à la Blackwell Academy', 'Pour résoudre un mystère local'],
-    correct: 2
-  },
-  {
-    question: 'Quel est le nom de la mère de Max ?',
-    answers: ['Joyce', 'Diana', 'Sarah', 'Vanessa'],
-    correct: 3
-  },
-  {
-    question: 'Qui est le beau-père de Chloé ?',
-    answers: ['Proviseur Wells', 'Mark Jefferson', 'David Madsen', 'Sean Prescott'],
-    correct: 2
-  }
+  { question: 'Quel est le nom complet de Max ?', answers: ['Maxine Caulfield', 'Max Caulfield', 'Maxine Carter', 'Max Carter'], correct: 0 },
+  { question: 'Quel est le super pouvoir de Max ?', answers: ['Télékinésie', 'Voyage dans le temps', 'Télépathie', 'Invisibilité'], correct: 1 },
+  { question: 'Quel est le nom de l\'amie d\'enfance de Max ?', answers: ['Rachel Amber', 'Victoria Chase', 'Kate Marsh', 'Chloé Price'], correct: 3 },
+  { question: 'Quel est le nom de l\'école que Max fréquente ?', answers: ['Arcadia Bay School', 'Pacific Northwest School', 'Blackwell Academy', 'West Coast Academy'], correct: 2 },
+  { question: 'Comment s\'appelle la ville où se déroule l\'histoire ?', answers: ['Arcadia Bay', 'Bright Falls', 'Twin Peaks', 'Silent Hill'], correct: 0 },
+  { question: 'Quelle est la passion artistique de Max ?', answers: ['Peinture', 'Photographie', 'Écriture', 'Musique'], correct: 1 },
+  { question: 'Quelle est la principale cause des conflits entre Max et Chloé ?', answers: ['Le déménagement de Max', 'Les secrets de Chloé', 'La disparition de Rachel', 'Les relations amoureuses'], correct: 0 },
+  { question: 'Pourquoi Max retourne-t-elle à Arcadia Bay après plusieurs années d\'absence ?', answers: ['Pour renouer avec sa meilleure amie Chloé', 'Pour un projet de photographie', 'Pour suivre des cours à la Blackwell Academy', 'Pour résoudre un mystère local'], correct: 2 },
+  { question: 'Quel est le nom de la mère de Max ?', answers: ['Joyce', 'Diana', 'Sarah', 'Vanessa'], correct: 3 },
+  { question: 'Qui est le beau-père de Chloé ?', answers: ['Proviseur Wells', 'Mark Jefferson', 'David Madsen', 'Sean Prescott'], correct: 2 }
 ];
 
 const QuizPage = () => {
   const dispatch = useDispatch();
   const quizState = useSelector(state => state.quiz);
 
-  // Conserver l'ordre des questions mélangées
   const [questions, setQuestions] = useState(() => {
     const savedQuestionsOrder = sessionStorage.getItem('questionsOrder');
     if (savedQuestionsOrder) {
@@ -83,6 +42,7 @@ const QuizPage = () => {
       return shuffledQuestions;
     }
   });
+
   const [showResults, setShowResults] = useState(false);
 
   useEffect(() => {
@@ -91,10 +51,7 @@ const QuizPage = () => {
     const savedCorrectAnswers = sessionStorage.getItem('correctAnswers');
     const savedShowResults = sessionStorage.getItem('showResults') === 'true';
 
-    if (savedResponses) {
-      dispatch(setResponses(JSON.parse(savedResponses)));
-    }
-    
+    if (savedResponses) dispatch(setResponses(JSON.parse(savedResponses)));
     if (savedResults && savedCorrectAnswers) {
       dispatch(setResults(JSON.parse(savedResults), parseInt(savedCorrectAnswers)));
       setShowResults(savedShowResults);
@@ -102,10 +59,7 @@ const QuizPage = () => {
   }, [dispatch]);
 
   const handleResponseChange = (index, answer) => {
-    const newResponses = {
-      ...quizState.responses,
-      [index]: answer
-    };
+    const newResponses = { ...quizState.responses, [index]: answer };
     dispatch(setResponses(newResponses));
     sessionStorage.setItem('quizResponses', JSON.stringify(newResponses));
   };
@@ -142,10 +96,7 @@ const QuizPage = () => {
 
   const handleReset = () => {
     dispatch(resetQuiz());
-    sessionStorage.removeItem('quizResponses');
-    sessionStorage.removeItem('quizResults');
-    sessionStorage.removeItem('correctAnswers');
-    sessionStorage.removeItem('showResults');
+    sessionStorage.clear();
     const shuffledQuestions = shuffleArray([...initialQuestions]);
     setQuestions(shuffledQuestions);
     sessionStorage.setItem('questionsOrder', JSON.stringify(shuffledQuestions));
@@ -153,12 +104,8 @@ const QuizPage = () => {
   };
 
   return (
-    <div>
-      {/* Message d'information pour certains navigateurs */}
-      <div className="info-message">
-        <p><strong>Info :</strong> Sur certains navigateurs (comme Brave, Safari et Internet Explorer), placez votre curseur sur le texte, pas sur le rond, pour sélectionner votre réponse.</p>
-      </div>
-
+    <div className="quiz-page">
+      
       {!showResults ? (
         <Quiz
           questions={questions}
@@ -171,7 +118,7 @@ const QuizPage = () => {
           results={quizState.results}
           correctAnswers={quizState.correctAnswers}
           totalQuestions={initialQuestions.length}
-          questions={questions} // Passer les questions dans l'ordre mélangé au Result
+          questions={questions}
           onReset={handleReset}
         />
       )}
