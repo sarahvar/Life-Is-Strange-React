9ef9867ee13886fa7d565a0623d2a5ed34904493
sarahvar <sarahvarin95@gmail.com>
2024-11-11 23:03:57 +0100

    MemoryGame-css

--- src/context/MemoryGameContext.jsx
+++ src/context/MemoryGameContext.jsx
@@ -12,29 +12,26 @@ export const MemoryGameProvider = ({ children }) => {
     { id: 3, src: '/assets/Rachel-Amber.webp' },
     { id: 4, src: '/assets/Warren-Graham.webp' },
     { id: 5, src: '/assets/David-Madsen.jpg' },
-    { id: 13, src: '/assets/William-Price.png' }
+    { id: 6, src: '/assets/William-Price.png' },
+    { id: 7, src: '/assets/Kate-Marsh.jpg' },
+
   ];
 
   const mediumImages = [
     ...easyImages,
-    { id: 6, src: '/assets/Kate-Marsh.jpg' },
-    { id: 7, src: '/assets/Frank-Bowers.webp' },
-    { id: 8, src: '/assets/Joyce-Madsen.jpg' },
-    { id: 9, src: '/assets/Nathan-Prescott.png' },
+    { id: 8, src: '/assets/Frank-Bowers.webp' },
+    { id: 9, src: '/assets/Joyce-Madsen.jpg' },
     { id: 10, src: '/assets/Victoria-Chase.webp' },
-    { id: 11, src: '/assets/Rayan-Caufield.png' },
   ];
 
   const hardImages = [
     ...mediumImages,
-    { id: 11, src: '/assets/Rayan-Caufield.png' },
+    { id: 11, src: '/assets/Nathan-Prescott.png' },
     { id: 12, src: '/assets/Vanessa-Caulfield.webp' },
-    { id: 13, src: '/assets/William-Price.png' },
-    { id: 14, src: '/assets/Mark_jefferson.webp' },
-    { id: 15, src: '/assets/Max-Biche.webp' },
-    { id: 16, src: '/assets/Home.jpg'},
+    { id: 13, src: '/assets/Mark_jefferson.webp' },
+    { id: 14, src: '/assets/Rayan-Caufield.png'},
   ];
-
+  
   const [difficulty, setDifficulty] = useState('easy'); // Difficulté par défaut à 'easy'
   const [cards, setCards] = useState([]);
   const [flippedCards, setFlippedCards] = useState([]);
